4912335180c8510a9471a54e9f106b318042ba4d
Otto von Sperling <otto.sperling@transferwise.com>
2025-11-22 20:19:13 +0200

    Load sample graph data in connected components example

--- notebooks/connected_components.py
+++ notebooks/connected_components.py
@@ -8,6 +8,7 @@ app = marimo.App(width="medium")
 def _():
     import os
     from pyspark.sql import SparkSession
+    from graphframes.examples import Graphs
 
 
     def get_spark_session() -> SparkSession:
@@ -17,19 +18,31 @@ def _():
         spark_session = SparkSession.builder.appName("Demo: Connected Components").getOrCreate()
         spark_session.sparkContext.setLogLevel("ERROR")
         return spark_session
-    return (get_spark_session,)
+    return Graphs, get_spark_session
 
 
 @app.cell
-def _(get_spark_session):
+def _(Graphs, get_spark_session):
     spark = get_spark_session()
+    graph = Graphs(spark).friends()
     spark
-    return (spark,)
+    return (graph,)
 
 
 @app.cell
-def _(spark):
-    spark.sql("select 1").show()
+def _(graph):
+    graph.vertices
+    return
+
+
+@app.cell
+def _(graph):
+    graph.edges
+    return
+
+
+@app.cell
+def _():
     return
 
 
